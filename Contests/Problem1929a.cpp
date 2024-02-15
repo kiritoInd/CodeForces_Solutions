08599e4d0e28c7abb80ccc480042c6c6928123da
kiritoInd <shreyansh69420@gmail.com>
2024-02-15 22:08:10 +0530

    15-02-2024

---
+++ Contests/Problem1929a.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <list>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <utility>
+#include <climits>
+
+using namespace std;
+#define  int          long long
+#define  sz(s)        (int)s.size()
+#define  all(v)       (v).begin(),(v).end()
+
+template <typename A, typename B> ostream& operator<< (ostream &cout, pair<A, B> const &p) { return cout << '(' << p.first << ',' << p.second << ')'; }
+template <typename A, typename B> istream& operator>> (istream& cin, pair<A, B> &p) {cin >> p.first; return cin >> p.second;}
+template <typename A> ostream& operator<< (ostream &cout, vector<A> const &v) {cout << '['; for(int i = 0; i < v.size(); i++) {if (i) cout << ','; cout << v[i];} return cout << ']';}
+template <typename A> istream& operator>> (istream& cin, vector<A> &x){for(int i = 0; i < x.size()-1; i++) cin >> x[i]; return cin >> x[x.size()-1];}
+template <typename A, typename B> A amax (A &a, B b){ if (b > a) a = b ; return a; }
+template <typename A, typename B> A amin (A &a, B b){ if (b < a) a = b ; return a; }
+
+const long long mod = 1e9+7;
+const long long inf = 1e18;
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    cin>>nums;
+    int maxe = INT_MIN;
+    int mine = INT_MAX;
+
+    for(int x : nums) {
+        maxe = max(x ,  maxe);
+        mine = min(x , mine);
+    }
+    cout<< (maxe - mine) <<endl;
+    return ;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
