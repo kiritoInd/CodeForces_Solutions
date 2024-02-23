8ce2e1c09931f6a8c23370a042c972f903bf988d
kiritoInd <shreyansh69420@gmail.com>
2024-02-23 18:40:21 +0530

    23-0202024

--- notSolved/problem1922c.cpp
+++ notSolved/problem1922c.cpp
@@ -60,7 +60,7 @@ int solve() {
     for (int i = n - 2; i >= 0; i--)
     {
         if (mp[i] != 1)  right[i] = right[i + 1] + abs(arr[i] - arr[i + 1]);
-        else      right[i] = 1 + right[i + 1];
+        else right[i] = 1 + right[i + 1];
         
     }
 
