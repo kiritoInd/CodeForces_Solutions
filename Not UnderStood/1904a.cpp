7d8d66c76c0e59bbb327ef7122328f6454c3fc9a
kiritoInd <shreyansh69420@gmail.com>
2024-02-29 23:39:27 +0530

    29-02-2024

---
+++ Not UnderStood/1904a.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <list>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+
+
+using namespace std;
+#define  int          long long
+#define  sz(s)        (int)s.size()
+#define  all(v)       (v).begin(),(v).end()
+#define  enl  	  '\n'
+
+template <typename A, typename B> ostream& operator<< (ostream &cout, pair<A, B> const &p) { return cout << '(' << p.first << ',' << p.second << ')'; }
+template <typename A, typename B> istream& operator>> (istream& cin, pair<A, B> &p) {cin >> p.first; return cin >> p.second;}
+template <typename A> ostream& operator<< (ostream &cout, vector<A> const &v) {cout << '['; for(int i = 0; i < v.size(); i++) {if (i) cout << ','; cout << v[i];} return cout << ']';}
+template <typename A> istream& operator>> (istream& cin, vector<A> &x){for(int i = 0; i < x.size()-1; i++) cin >> x[i]; return cin >> x[x.size()-1];}
+template <typename A, typename B> A amax (A &a, B b){ if (b > a) a = b ; return a; }
+template <typename A, typename B> A amin (A &a, B b){ if (b < a) a = b ; return a; }
+
+const long long mod = 1e9+7;
+const long long inf = 1e18;
+
+int solve() {
+        int a,b,xk,yk,xq,yq;
+        cin>>a>>b>>xk>>yk>>xq>>yq;
+        int res=0;
+
+        int x = xk + a;
+        int y = yk + b;
+        if (abs(xq - x) == a && abs(yq - y) == b) res++; else
+        if (abs(xq - x) == b && abs(yq - y) == a) res++;
+ 
+        x = xk - a;
+        y = yk - b;
+        if (abs(xq - x) == a && abs(yq - y) == b) res++; else
+        if (abs(xq - x) == b && abs(yq - y) == a) res++;
+ 
+        x = xk - a;
+        y = yk + b;
+        if (abs(xq - x) == a && abs(yq - y) == b) res++; else
+        if (abs(xq - x) == b && abs(yq - y) == a) res++;
+ 
+        x = xk + a;
+        y = yk - b;
+        if (abs(xq - x) == a && abs(yq - y) == b) res++; else
+        if (abs(xq - x) == b && abs(yq - y) == a) res++;
+ 
+        if (a!=b){
+        x = xk - b;
+        y = yk - a;
+        if (abs(xq - x) == a && abs(yq - y) == b) res++; else
+        if (abs(xq - x) == b && abs(yq - y) == a) res++;
+ 
+        x = xk + b;
+        y = yk - a;
+        if (abs(xq - x) == a && abs(yq - y) == b) res++; else
+        if (abs(xq - x) == b && abs(yq - y) == a) res++;
+ 
+        x = xk - b;
+        y = yk + a;
+        if (abs(xq - x) == a && abs(yq - y) == b) res++; else
+        if (abs(xq - x) == b && abs(yq - y) == a) res++;
+ 
+        x = xk + b;
+        y = yk + a;
+        if (abs(xq - x) == a && abs(yq - y) == b) res++; else
+        if (abs(xq - x) == b && abs(yq - y) == a) res++;}
+        cout<<res<<'\n';
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
