d898843cff1b58672a4c819f01277193451cece2
kiritoInd <shreyansh69420@gmail.com>
2024-03-07 22:43:48 +0530

    07-02-2024

---
+++ Contests/problem1913b.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <list>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+
+
+using namespace std;
+#define  int          long long
+#define  sz(s)        (int)s.size()
+#define  all(v)       (v).begin(),(v).end()
+#define  enl  	  '\n'
+
+template <typename A, typename B> ostream& operator<< (ostream &cout, pair<A, B> const &p) { return cout << '(' << p.first << ',' << p.second << ')'; }
+template <typename A, typename B> istream& operator>> (istream& cin, pair<A, B> &p) {cin >> p.first; return cin >> p.second;}
+template <typename A> ostream& operator<< (ostream &cout, vector<A> const &v) {cout << '['; for(int i = 0; i < v.size(); i++) {if (i) cout << ','; cout << v[i];} return cout << ']';}
+template <typename A> istream& operator>> (istream& cin, vector<A> &x){for(int i = 0; i < x.size()-1; i++) cin >> x[i]; return cin >> x[x.size()-1];}
+template <typename A, typename B> A amax (A &a, B b){ if (b > a) a = b ; return a; }
+template <typename A, typename B> A amin (A &a, B b){ if (b < a) a = b ; return a; }
+
+const long long mod = 1e9+7;
+const long long inf = 1e18;
+
+int solve() {
+    string s;
+    cin>>s;
+    int count1 = 0;
+    int count0 = 0;
+    for(char c : s){
+        if(c == '0') count0++;
+        else count1++;
+    }
+    int i = 0;
+    string t = s;
+    while(i < s.size()){
+        if(count1 > 0 && s[i] == '0'){
+            s[i] = '1';
+            count1--;
+        }else if(count0 > 0 && s[i] == '1'){
+            s[i] = '0';
+            count0--;
+        }
+        i++;
+    }
+    int j = 0;
+    for(j = 0 ; j< s.size() ;j++){
+        if(s[j] == t[j]){
+            break;
+        }
+    }
+    cout<<s.size() - j<<enl;
+    return 0;
+    
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
