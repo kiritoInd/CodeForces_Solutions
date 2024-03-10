24b4eaa9fd27d1c33695687b3e86bb9b602a499d
kiritoInd <shreyansh69420@gmail.com>
2024-03-10 18:33:51 +0530

    10-03-2024

---
+++ Contests/problem158a.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <list>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+
+
+using namespace std;
+#define  int          long long
+#define  sz(s)        (int)s.size()
+#define  all(v)       (v).begin(),(v).end()
+#define  enl  	  '\n'
+
+template <typename A, typename B> ostream& operator<< (ostream &cout, pair<A, B> const &p) { return cout << '(' << p.first << ',' << p.second << ')'; }
+template <typename A, typename B> istream& operator>> (istream& cin, pair<A, B> &p) {cin >> p.first; return cin >> p.second;}
+template <typename A> ostream& operator<< (ostream &cout, vector<A> const &v) {cout << '['; for(int i = 0; i < v.size(); i++) {if (i) cout << ','; cout << v[i];} return cout << ']';}
+template <typename A> istream& operator>> (istream& cin, vector<A> &x){for(int i = 0; i < x.size()-1; i++) cin >> x[i]; return cin >> x[x.size()-1];}
+template <typename A, typename B> A amax (A &a, B b){ if (b > a) a = b ; return a; }
+template <typename A, typename B> A amin (A &a, B b){ if (b < a) a = b ; return a; }
+
+const long long mod = 1e9+7;
+const long long inf = 1e18;
+
+int solve() {
+    int n, k;
+    cin >> n >> k;
+    int ans = 0;
+    int arr[10000];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] >= arr[k - 1] && arr[i] > 0)
+        {
+            ans++;
+        }
+    }
+    cout << ans;
+    return 0;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+        solve();
+}
