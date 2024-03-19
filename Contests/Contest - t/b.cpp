912007f1be94249bf03de7dcf5cbb6c15b9cc9d0
kiritoInd <shreyansh69420@gmail.com>
2024-03-19 16:06:08 +0530

    19-03-2024

---
+++ Contests/Contest - t/b.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <list>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+
+
+using namespace std;
+#define  int          long long
+#define  sz(s)        (int)s.size()
+#define  all(v)       (v).begin(),(v).end()
+#define  enl  	  '\n'
+
+template <typename A, typename B> ostream& operator<< (ostream &cout, pair<A, B> const &p) { return cout << '(' << p.first << ',' << p.second << ')'; }
+template <typename A, typename B> istream& operator>> (istream& cin, pair<A, B> &p) {cin >> p.first; return cin >> p.second;}
+template <typename A> ostream& operator<< (ostream &cout, vector<A> const &v) {cout << '['; for(int i = 0; i < v.size(); i++) {if (i) cout << ','; cout << v[i];} return cout << ']';}
+template <typename A> istream& operator>> (istream& cin, vector<A> &x){for(int i = 0; i < x.size()-1; i++) cin >> x[i]; return cin >> x[x.size()-1];}
+template <typename A, typename B> A amax (A &a, B b){ if (b > a) a = b ; return a; }
+template <typename A, typename B> A amin (A &a, B b){ if (b < a) a = b ; return a; }
+
+const long long mod = 1e9+7;
+const long long inf = 1e18;
+
+typedef long long ll;
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    return (a / gcd(a, b)) * b;
+}
+
+int solve() {
+    int a , b , m;
+    cin>>a>>b>>m;
+    int lcm_ab = lcm(a, b);
+    int total_fireworks = (m / a) + (m / b) + 1;
+    int visible_fireworks = max((m / a) + ((m % a) >= (lcm_ab - b)), (m / b) + ((m % b) >= (lcm_ab - a)));
+    return max(total_fireworks, visible_fireworks) + (int)1;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        cout<<solve()<<enl;
+    }
+}
