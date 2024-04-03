e246a18592bf49fdcb7cdf23c615c0eddd6afc4d
kiritoInd <shreyansh69420@gmail.com>
2024-04-03 22:14:05 +0530

    3-03-2024

---
+++ Codechef/Starters128/a.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <list>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+int main() {
+	// your code goes here
+    int x , y;
+    cin>>x>>y;
+    if(x + y >= 2000){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
---
+++ Codechef/Starters128/c.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <list>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+
+
+using namespace std;
+#define  int          long long
+#define  sz(s)        (int)s.size()
+#define  all(v)       (v).begin(),(v).end()
+#define  enl  	  '\n'
+
+vector<int> count_divisors_to_n(int n){
+    vector<int> res(n + 1);
+    for(int i = 1 ; i <= n ; i++){
+        for(int j = i ; j <= n; j += i){
+            res[j]++;
+        }
+    }
+    return res;
+}
+int count_divisors(int n){
+    int count = 0;
+    for(int i = 1 ; i * i <= n;i++){
+        if(n % i == 0){
+            count++;
+            if(n/i != i){
+                count++;
+            }
+        }
+
+    }
+    return count;
+}
+template <typename A, typename B> ostream& operator<< (ostream &cout, pair<A, B> const &p) { return cout << '(' << p.first << ',' << p.second << ')'; }
+template <typename A, typename B> istream& operator>> (istream& cin, pair<A, B> &p) {cin >> p.first; return cin >> p.second;}
+template <typename A> ostream& operator<< (ostream &cout, vector<A> const &v) {cout << '['; for(int i = 0; i < v.size(); i++) {if (i) cout << ','; cout << v[i];} return cout << ']';}
+template <typename A> istream& operator>> (istream& cin, vector<A> &x){for(int i = 0; i < x.size()-1; i++) cin >> x[i]; return cin >> x[x.size()-1];}
+template <typename A, typename B> A amax (A &a, B b){ if (b > a) a = b ; return a; }
+template <typename A, typename B> A amin (A &a, B b){ if (b < a) a = b ; return a; }
+
+const long long mod = 1e9+7;
+const long long inf = 1e18;
+
+int solve() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    cin>>a;
+
+    sort(a.begin(), a.end());
+    vector<int> s = a;
+    int mx = a[n - 1];
+    a[0] = 0;
+    for(int i = 1 ; i < n;i++){
+        a[i] = a[i - 1] + 1;
+    }
+    int res = 0;
+    for(int i = 0; i < n;i++){
+        res += abs(s[i] - a[i]);
+    }
+    cout<<res<<enl;
+    return 0;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
